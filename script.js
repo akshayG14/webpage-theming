1fdae6ab3b390782fb7dfccb3d873a3b5c4b9e39
AkshayG <akshayg14@gmail.com>
2020-09-07 22:18:06 +0530

    Refactored the JS code

--- script.js
+++ script.js
@@ -11,21 +11,25 @@ toggleSwitch.addEventListener('change', switchTheme);
 
 // Theme Switch function
 function switchTheme(event) {
-  (event.target.checked) ? targetTheme('dark') : targetTheme('light');
+  (event.target.checked) ? setTheme('dark') : setTheme('light');
 }
 
-function targetTheme(theme) {
+// Based on the input, the webpage is set to dark/light theme mode
+function setTheme(theme) {
   const navColor = (theme === 'light') ? '255 255 255' : '0 0 0';
   const textBoxColor = theme === 'light' ? '0 0 0' : '255 255 255';
   const capitalizedText = theme[0].toUpperCase() + theme.slice(1); // Capitalize the first character
 
   document.documentElement.setAttribute('data-theme', `${theme}`);
+
   nav.style.backgroundColor = `rgb(${navColor} / 50%)`;
   textBox.style.backgroundColor = `rgb(${textBoxColor} / 50%)`;
   toggleIcon.children[0].textContent = `${capitalizedText} Mode`;
+
   image1.src = `img/undraw_proud_coder_${theme}.svg`;
   image2.src = `img/undraw_feeling_proud_${theme}.svg`;
   image3.src = `img/undraw_conceptual_idea_${theme}.svg`;
+
   toggleIcon.children[1].classList.replace(
     (theme === 'dark') ? 'fa-sun' : 'fa-moon',
     (theme === 'dark') ? 'fa-moon' : 'fa-sun'
@@ -37,7 +41,7 @@ function targetTheme(theme) {
 // Check localStorage for currentTheme
 const currentTheme = localStorage.getItem('currentTheme');
 if (currentTheme) {
-  targetTheme(currentTheme);
+  setTheme(currentTheme);
   if (currentTheme === 'dark') {
     toggleSwitch.checked = true;
   }
